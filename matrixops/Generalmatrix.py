49b74168a228e55e71ac914d7d9ee64d81970ff0
Vítor Scheifler de Almeida <vitor.almeida@braslux.com.br>
2022-02-25 14:03:17 -0300

    Adding a transpose function and a matrix converter

--- matrixops/Generalmatrix.py
+++ matrixops/Generalmatrix.py
@@ -3,12 +3,16 @@ from random import random
 class Matrix:
 
     def __init__(self, m=2, n=2):
+        """
+        """
 
         self.m = m
         self.n = n
         self.data = []
 
     def make_rand_mtx(self, m, n):
+        """
+        """
 
         self.m = m
         self.n = n
@@ -18,4 +22,26 @@ class Matrix:
 
         return mtx
 
-    
+    def to_mtx(self, list_of_lists):
+        """
+        """
+
+        check_sizes = [len(list) for list in list_of_lists]
+        set1 = set(check_sizes)
+
+        if len(set1) > 1:
+            raise ValueError('The matrix input has lists of different shapes! Please check the input')
+
+        self.m = len(list_of_lists)
+        self.n = len(list_of_lists[0])
+        self.data = list_of_lists
+
+    def T(self):
+        """
+        """
+
+        self.data = list(map(list, zip(*self.data)))
+        self.m = len(self.data)
+        self.n = len(self.data[0])
+
+        return self.data
