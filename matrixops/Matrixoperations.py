ed99f587bc35b4e26d0ff58e0cba1a3831f81ce3
Vitor-Almeida <vitorscheiflerdealmeida@gmail.com>
2022-02-26 11:27:41 -0300

    Creating an add operation and representation for Matrices

--- matrixops/Matrixoperations.py
+++ matrixops/Matrixoperations.py
@@ -1,5 +1,5 @@
 
-from .Generalmatrix import Matrix
+from MtxOps import Matrix
 
 class Operations(Matrix):
 
@@ -7,4 +7,10 @@ class Operations(Matrix):
 
         Matrix.__init__(self, m, n)
 
-    
+m1 = Matrix()
+m1.to_mtx([[1,2], [3, 3], [4,6]])
+m2 = Matrix()
+m2.to_mtx([[1,1], [2, 2], [3, 3]])
+print(m1)
+print(m2.shape())
+print(m1 + m2)
--- matrixops/MtxOps.py
+++ matrixops/MtxOps.py
@@ -45,3 +45,23 @@ class Matrix:
         self.n = len(self.data[0])
 
         return self.data
+
+    def shape(self):
+
+        return self.m, self.n
+
+    def __repr__(self):
+        return "Matrix with {} rows and {} columns.".format(self.m, self.n)
+
+    def __add__(self, other):
+
+        result = Matrix()
+
+        if self.shape() != other.shape():
+            raise ValueError("The matrices have different orders!")
+
+        result.data = [list(map(sum, zip(*t)))
+                       for t in zip(self.data, other.data)]
+        result.m = self.m
+        result.n = self.n
+        return result.data
